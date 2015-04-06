c4cb0c7e40adbc36f3b59d2e462a740671d705c6
ranjanvittal <ranjan@Ranjans-MacBook-Air.local>
2015-04-07 01:02:16 +0530

    decently done function inlining

---
+++ Modern Techniques in Compilers/CS12B050_5/P5/P5.java
@@ -0,0 +1,24 @@
+import syntaxtree.*;
+import visitor.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class P5 {
+   public static void main(String [] args) {
+      try {
+         FunkyTacoJavaParser funkyTacoJavaParser = new FunkyTacoJavaParser(System.in);
+         Node root1 = funkyTacoJavaParser.Goal();
+         //System.out.println("Program Parsed Successfully");
+         Object passOn = (Object) root1.accept(new GJDepthFirst<Object, Object>(), null);
+         root1.accept(new GJDepth2<Object, Object>(), passOn);
+
+      }
+      catch (ParseException e) {
+         System.out.println(e.toString());
+      }
+   }
+}
+
+
---
+++ Modern Techniques in Compilers/CS12B050_5/P5/gen.java
@@ -0,0 +1,216 @@
+class funkyTacoT1 {
+public static void main(String[] funkyTacoa) {
+funkyTacoFac3 funkyTacofunkyTacon;
+int funkyTacofunkyTacot;
+int funkyTacofunkyTacot1;
+int funkyTacofunkyTacoid1;
+int funkyTacofunkyTaconine_thousand;
+funkyTacoFac3 temp0;
+int temp1;
+int[] temp2;
+int temp3;
+int temp4;
+int temp5;
+int temp6;
+int temp7;
+int temp8;
+int temp9;
+int[] temp10;
+int[] temp11;
+int temp12;
+int temp13;
+int temp14;
+int temp15;
+int temp16;
+funkyTacoFac3 temp17;
+int temp18;
+int[] temp19;
+int temp20;
+int temp21;
+int temp22;
+int temp23;
+int temp24;
+int temp25;
+funkyTacon = new funkyTacoFac3();
+funkyTacot = 64;
+funkyTaconine_thousand = 9000;
+funkyTacon.funkyTaconum = funkyTaconine_thousand;
+funkyTacoid1 = 23;
+
+{
+temp1 = funkyTacon.get_funkyTacoFac3_funkyTacogg_13();
+temp2 = funkyTacon.get_funkyTacoFac3_funkyTacoa_13();
+temp3 = funkyTacon.get_funkyTacoFac3_funkyTaconum_13();
+temp4 = null;
+temp15 = 0;
+temp16 = 0;
+temp14 = 0;
+temp13 = temp3+temp3;
+temp2 = new int[temp13];
+temp10 = new int[temp13];
+temp11 = new int[temp13];
+for(temp5 = 0;temp5<64;temp5 = temp5+1)
+for(temp6 = 0;temp6<64;temp6 = temp6+1)
+{
+temp15 = temp5*10;
+temp15 = temp15*2;
+temp16 = temp5*6;
+temp16 = temp16+1;
+temp16 = temp2[temp16];
+temp2[temp15] = temp16;
+
+}
+for(temp5 = 0;temp5<64;temp5 = temp5+1)
+for(temp6 = 0;temp6<64;temp6 = temp6+1)
+{
+temp15 = temp5;
+temp15 = temp15*temp15;
+temp16 = temp5*6;
+temp16 = temp16+1;
+temp16 = temp2[temp16];
+temp2[temp15] = temp16;
+
+}
+
+{
+temp18 = temp0.get_funkyTacoFac3_funkyTacogg_13();
+temp19 = temp0.get_funkyTacoFac3_funkyTacoa_13();
+temp20 = temp0.get_funkyTacoFac3_funkyTaconum_13();
+for(temp21 = 0;temp21<64;temp21 = temp21+1)
+for(temp22 = 0;temp22<64;temp22 = temp22+1)
+{
+temp23 = temp21*2;
+temp23 = temp23+temp22;
+temp24 = temp22*2;
+temp24 = temp24+temp21;
+temp24 = temp19[temp24];
+temp24 = temp24+1;
+temp19[temp23] = temp24;
+
+}
+temp25 = 0;
+temp1 = temp25;
+temp0.funkyTacogg = temp1;
+
+}
+temp14 = temp2[60];
+funkyTacot1 = temp14;
+
+}
+System.out.println(funkyTacot1);
+
+}
+}
+class funkyTacoFac3
+{
+int funkyTacofunkyTaconum;
+int[] funkyTacofunkyTacoa;
+int funkyTacofunkyTacogg;
+public int get_funkyTacoFac3_funkyTaconum_13() {
+return funkyTaconum;
+}
+public int[] get_funkyTacoFac3_funkyTacoa_13() {
+return funkyTacoa;
+}
+public int get_funkyTacoFac3_funkyTacogg_13() {
+return funkyTacogg;
+}
+public int funkyTacoassignArray() { 
+int funkyTacofunkyTacoi;
+int funkyTacofunkyTacoj;
+int funkyTacofunkyTacok1;
+int funkyTacofunkyTacok2;
+int funkyTacofunkyTacozero;
+for(funkyTacoi = 0;funkyTacoi<64;funkyTacoi = funkyTacoi+1)
+for(funkyTacoj = 0;funkyTacoj<64;funkyTacoj = funkyTacoj+1)
+{
+funkyTacok1 = funkyTacoi*2;
+funkyTacok1 = funkyTacok1+funkyTacoj;
+funkyTacok2 = funkyTacoj*2;
+funkyTacok2 = funkyTacok2+funkyTacoi;
+funkyTacok2 = funkyTacoa[funkyTacok2];
+funkyTacok2 = funkyTacok2+1;
+funkyTacoa[funkyTacok1] = funkyTacok2;
+
+}
+funkyTacozero = 0;
+return funkyTacozero;
+}
+public int funkyTacoComputeFac3(int funkyTaconum) { 
+int funkyTacofunkyTacoi;
+int funkyTacofunkyTacoj;
+int funkyTacofunkyTacok;
+int funkyTacofunkyTacosum;
+int funkyTacofunkyTacosum2;
+int[] funkyTacofunkyTacob;
+int[] funkyTacofunkyTacoc;
+int funkyTacofunkyTacokk;
+int funkyTacofunkyTacom;
+int funkyTacofunkyTacoret;
+int funkyTacofunkyTacok1;
+int funkyTacofunkyTacok2;
+funkyTacoFac3 temp26;
+int temp27;
+int[] temp28;
+int temp29;
+int temp30;
+int temp31;
+int temp32;
+int temp33;
+int temp34;
+funkyTacok1 = 0;
+funkyTacok2 = 0;
+funkyTacoret = 0;
+funkyTacom = funkyTaconum+funkyTaconum;
+funkyTacoa = new int[funkyTacom];
+funkyTacob = new int[funkyTacom];
+funkyTacoc = new int[funkyTacom];
+for(funkyTacoi = 0;funkyTacoi<64;funkyTacoi = funkyTacoi+1)
+for(funkyTacoj = 0;funkyTacoj<64;funkyTacoj = funkyTacoj+1)
+{
+funkyTacok1 = funkyTacoi*10;
+funkyTacok1 = funkyTacok1*2;
+funkyTacok2 = funkyTacoi*6;
+funkyTacok2 = funkyTacok2+1;
+funkyTacok2 = funkyTacoa[funkyTacok2];
+funkyTacoa[funkyTacok1] = funkyTacok2;
+
+}
+for(funkyTacoi = 0;funkyTacoi<64;funkyTacoi = funkyTacoi+1)
+for(funkyTacoj = 0;funkyTacoj<64;funkyTacoj = funkyTacoj+1)
+{
+funkyTacok1 = funkyTacoi;
+funkyTacok1 = funkyTacok1*funkyTacok1;
+funkyTacok2 = funkyTacoi*6;
+funkyTacok2 = funkyTacok2+1;
+funkyTacok2 = funkyTacoa[funkyTacok2];
+funkyTacoa[funkyTacok1] = funkyTacok2;
+
+}
+
+{
+temp27 = this.get_funkyTacoFac3_funkyTacogg_13();
+temp28 = this.get_funkyTacoFac3_funkyTacoa_13();
+temp29 = this.get_funkyTacoFac3_funkyTaconum_13();
+for(temp30 = 0;temp30<64;temp30 = temp30+1)
+for(temp31 = 0;temp31<64;temp31 = temp31+1)
+{
+temp32 = temp30*2;
+temp32 = temp32+temp31;
+temp33 = temp31*2;
+temp33 = temp33+temp30;
+temp33 = temp28[temp33];
+temp33 = temp33+1;
+temp28[temp32] = temp33;
+
+}
+temp34 = 0;
+funkyTacogg = temp34;
+
+}
+funkyTacoret = funkyTacoa[60];
+return funkyTacoret;
+}
+
+}
+
